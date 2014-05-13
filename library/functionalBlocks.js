a1966aa0614cfb6e81eaea227e72a67203e2c57e
Arabirim <aycan.bulbul@arabirim.com.tr>
2014-05-13 14:09:21 +0300

    Create functionalBlocks.js

---
+++ library/functionalBlocks.js
@@ -0,0 +1,11 @@
+    $(function (){
+        $('.leftBlock, .rightBlock').css( 'height', $( '.contentBlock' ).outerHeight());
+    });
+
+    function blockClose( blocks, obj ) {
+        $( 'body' ).toggleClass( blocks );
+    }
+
+    $( window ).resize( function (){
+        $('.leftBlock, .rightBlock').css( 'height', $( '.contentBlock' ).outerHeight() );
+    });
